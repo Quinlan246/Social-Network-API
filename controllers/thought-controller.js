6fdb9829a8b13c039c5fc65679fcb2959b1600d2
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-11-23 15:02:25 -0600

    added create thought in thought-controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -35,4 +35,26 @@ const thoughtController = {
             res.sendStatus(400);
         });
     },
+    createThought({ params, body }, res) {
+        Thought.create(body)
+          .then(({ _id }) => {
+            return User.findOneAndUpdate(
+              { _id: body.userId },
+              { $push: { thoughts: _id } },
+              { new: true }
+            );
+          })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              return res
+                .status(404)
+                .json({ message: "Thought created but no user with this id!" });
+            }
+    
+            res.json({ message: "Thought successfully created!" });
+          })
+          .catch((err) => res.json(err));
+      },
 }
+
+module.exports = thoughtController;
