a73193425bdeeb3916348dfa4806dcc64f30736c
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-11-23 21:00:31 -0600

    added create/update user in user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,7 +16,6 @@ const userController = {
         res.sendStatus(400);
       });
   },
-
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
       .populate({
@@ -40,6 +39,25 @@ const userController = {
         console.log(err);
         res.sendStatus(400);
       });
+  },
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
   },
 }
 
